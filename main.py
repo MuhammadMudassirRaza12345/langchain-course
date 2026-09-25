3c8d4040c6b65639a96a8f51418f0e5e1d3db688
mudassir786110 <786mudassirraza@gmail.com>
2026-09-25 21:14:35 +0500

    ice breaker 1

--- main.py
+++ main.py
@@ -9,31 +9,33 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
+# tavily = TavilyClient()
 
-tavily = TavilyClient()
-
-@tool
-def search(query: str) -> str:
-    """
-    Tool that searches over internet
-    Args:
-        query: The query to search for
-    Returns:
-        The search result
-    """
-    print(f"Searching for {query}")
-    return tavily.search(query=query)
+# @tool
+# def search(query: str) -> str:
+#     """
+#     Tool that searches over internet
+#     Args:
+#         query: The query to search for
+#     Returns:
+#         The search result
+#     """
+#     print(f"Searching for {query}")
+#     return tavily.search(query=query)
 
 
 llm = ChatOpenAI(model="gpt-5")
-tools = [search]
-agent = create_agent(model=llm,tools=tools)
+tools = [TavilySearch()]
+agent = create_agent(model=llm, tools=tools)
 
 
 def main():
     print("Hello from langchain-course!")
-    result = agent.invoke({"messages":HumanMessage(content="search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details")})
+    result = agent.invoke(
+        {"messages": HumanMessage(content="search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details?")}
+    )
     print(result)
 
 if __name__ == "__main__":
